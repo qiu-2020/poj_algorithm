e6529e4cb3584f925d1fdb1beccd6144421ea0b2
qiu-2020 <qiuziyuhnu@outlook.com>
2021-11-21 19:01:15 +0800

    0-1 pack

--- poj3624_2.cpp
+++ poj3624_2.cpp
@@ -2,17 +2,25 @@
 
 int N,M;
 int charm[3500][2];
-int ans[3500][400];
+int ans[12880] = {0};
 
 void bracklet(void){
-    
+    for(int i = 0;i <= M;i++)
+        if(charm[0][0] <= i)
+            ans[i] = charm[0][1];
+    for(int i = 1;i < N;i++){
+        for(int j = M;j >= charm[i][0];j--){
+            ans[j] = std::max(ans[j],ans[j - charm[i][0]] + charm[i][1]);
+        }
+    }
 }
 
 int main(){
     std::cin>>N>>M;
     for(int i = 0;i < N;i++)
         std::cin>>charm[i][0]>>charm[i][1];
-
+    bracklet();
+    std::cout<<ans[M]<<std::endl;
 
 
     return 0;
