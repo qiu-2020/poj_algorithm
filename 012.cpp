0fde094408affcffd23db6894e8bd6c51a49db53
qiu-2020 <qiuziyuhnu@outlook.com>
2021-11-27 14:45:04 +0800

    master

---
+++ 012.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int k;
+int ans[26][26] = {0};
+int a[16] = {0};
+
+int max_len(int i,int j){// i当前考察的数字，j代表上一个选择的数字
+    if(i == k){
+        
+    }
+
+}
+
+int main(){
+    std::cin>>k;
+    for(int i = 1;i <= k;++i){
+        std::cin>>a[i];
+    }
+
+
+    return 0;
+}
