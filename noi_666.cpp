587bae1484a38572fd860c0735ea3c00ad871732
qiu-2020 <qiuziyuhnu@outlook.com>
2021-12-06 14:13:37 +0800

    master

--- noi_666.cpp
+++ noi_666.cpp
@@ -4,21 +4,20 @@ int ans[15][15];
 int m,n;
 int apple(int n,int m){//n个苹果放m个盘子
     if(m > n) return apple(n,n);
-    if(m == 1) return 1;
-    if(n == 1) return 1;
+    if(m == 0) return 0;
+    if(n == 0) return 1;
     return apple(n - m,m) + apple(n,m - 1);
 }
 
 void apple_dp(void){
-    // for(int i = 0;i <= n;++i){
-    //     for(int j = 1;j <= m;++j){
-    //         if(i == 1) ans[i][j] = 1;
-    //         if(j == 1) ans[i][j] = 1;
-    //         if(i == 0) ans[i][j] = 1;
-    //         if(j > i) ans[i][j] = ans[i][i];
-    //         else ans[i][j] = ans[i - j][j] + ans[i][j - 1];
-    //     }
-    // }
+    for(int i = 0;i <= n;++i){
+        for(int j = 0;j <= m;++j){
+            if(j == 0) ans[i][j] = 0;
+            if(i == 0) ans[i][j] = 1;
+            if(i < j) ans[i][j] = ans[i][i];
+            else ans[i][j] = ans[i - j][j] + ans[i][j - 1];
+        }
+    }
 }
 
 int main(){
