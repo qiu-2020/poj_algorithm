16270843d961ad3c56f8fc549462f4880cbe6a56
qiu-2020 <qiuziyuhnu@outlook.com>
2022-03-21 14:47:11 +0800

    22_3_21

---
+++ leetcode128.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <map>
+using namespace std;
+
+
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        if(nums.size() == 0) return 0;
+        map<int,int> m;
+        int ans = 0;
+        for(vector<int>::iterator it = nums.begin();it != nums.end();++it){
+            if()
+        }
+    }
+};
+//[0,3,7,2,5,8,4,6,0,1]
+// 1 1 1 2        
+
+
+
+int main(){
+    vector<int> input1({100,4,200,1,3,2}),input2({0,3,7,2,5,8,4,6,0,1});
+
+
+    return 0;
+}
